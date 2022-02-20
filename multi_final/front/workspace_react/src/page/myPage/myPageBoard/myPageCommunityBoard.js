88f380e11c481a43a9d83dfd8ebc485aae7bd2f4
Yim <yimjh2309@gmail.com>
2022-02-20 23:41:05 +0900

    마이페이지 업데이트(미완성)

---
+++ multi_final/front/workspace_react/src/page/myPage/myPageBoard/myPageCommunityBoard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Sidebar from '../sidebar';
+import "./myPageCommunityBoard.scss";
+
+function myPageCommunityBoard(props) {
+    return (
+        <div className='myList'>
+            <Sidebar />
+            
+            <div className='board'>
+                asd
+            </div>
+        </div>
+    );
+}
+
+export default myPageCommunityBoard;
---
+++ multi_final/front/workspace_react/src/page/myPage/myPageBoard/myPageInformationModify.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from '../sidebar';
+
+const myPageInformationModify = () => {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    );
+};
+
+export default myPageInformationModify;
