596854835f49e99034323c3042c5fa51444eb996
juyounglimm <juu940428@gmail.com>
2022-03-01 21:18:51 +0900

    커밋

--- multi_final/front/workspace_react/src/App.js
+++ multi_final/front/workspace_react/src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import Main from "./page/Main";
 import MainBottomNevber from "./components/main/MainBottomNavber.js";
 import Login from "./page/login";
