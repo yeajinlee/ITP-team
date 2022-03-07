084c190a1a1264101136678fa63359eb427e617a
juyounglimm <juu940428@gmail.com>
2022-03-07 11:36:33 +0900

    메인수정

--- multi_final/front/workspace_react/src/components/main/MainImage.js
+++ multi_final/front/workspace_react/src/components/main/MainImage.js
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './css/MainImage.scss';
 import { Card, Table} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import communityBoradData from './jsonFile/CommunityBoradData.json';
 import { Link } from 'react-router-dom';
-import groupBoard from '../../page/communityGroup/GroupBoradData.json';
+//import groupBoard from '../../page/communityGroup/GroupBoradData.json';
+import axios from 'axios';
 
 const Mainimage = () => {
+
+    const[Groupdatas,setGroupdata]=useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const[Comdatas,setComdata]=useState([]);
+
+    useEffect(()=>{
+      const fetchGroupmain=async()=>{
+        try {
+       
+            setError(null);
+            
+            // loading 상태를 true
+            setLoading(true); 
+            
+            const response=await axios.get(`http://localhost:8085/group/recent`,null);
+            setGroupdata(response.data);
+            
+          }catch(e){
+            setError(e);
+        }
+        setLoading(false);
+      
+    
+  };
+  fetchGroupmain();
+  
+   },[]);
+
+   useEffect(()=>{
+    const fetchCommain=async()=>{
+      try {
+     
+          setError(null);
+          
+          // loading 상태를 true
+          setLoading(true); 
+          
+          const response=await axios.get(`http://localhost:8085/com/recent`,null);
+          setComdata(response.data);
+          
+        }catch(e){
+          setError(e);
+      }
+      setLoading(false);
+    
+  
+};
+fetchCommain();
+
+ },[]);
+   if (loading) return <div>로딩중..</div>;
+   if (error) return <div>에러가 발생했습니다</div>;
+  if (!Groupdatas) return null;
+  if (!Comdatas) return null;
     return (
 
       
@@ -48,21 +104,25 @@ const Mainimage = () => {
             }}>
               모임찾기
               </p>
-          <div id='groupLine' className='groupBoardLine'>
+
+             <div id='groupLine' className='groupBoardLine'>
+          
+                 {Groupdatas.map((Groupdata) =>
             <Card style={{ width: '18rem' }}>
-              {groupBoard.group.map((n,index) =>
-              <Card.Body>
-                <Card.Img variant='top' src={n.img} />
-                <Card.Title className='title'>{n.title}</Card.Title>
-                <Card.Text className='cardText'>{n.content}</Card.Text>
+
+              <Card.Body key={Groupdata.g_no}>
+                <Card.Img variant='top' src={Groupdata.g_img} />
+                <Card.Title className='title'>{Groupdata.g_title}</Card.Title>
+                <Card.Text className='cardText'>{Groupdata.g_subtitle}</Card.Text>
                 <br />
                 <Card.Body className='bodyLink'>
-                  <Card.Link className='link' href="#">{n.writer}</Card.Link>
-                  <Card.Link className='link' href="#">{n.topic}</Card.Link>
+                  <Card.Link className='link' href="#">{Groupdata.g_name}</Card.Link>
+                  <Card.Link className='link' href="#">{Groupdata.g_tag}</Card.Link>
                 </Card.Body>
               </Card.Body>
-            )}
+         
             </Card>
+               )}
           </div>
 
           <div id='mainBoard' className='boardTable'>
@@ -77,16 +137,16 @@ const Mainimage = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    {communityBoradData.community.map((n,index) => (
-                            <tr key={index}>
-                              <td>{n.no}</td>
+                    {Comdatas.map((Comdata) => (
+                            <tr key={Comdata.c_no}>
+                              <td>{Comdata.c_no}</td>
                               <td>
-                                <Link to={'/Community/'+n.no} style={{ textDecoration: 'none' }}>
-                                  {n.title}
+                                <Link to={'/Community/'+Comdata.c_no} style={{ textDecoration: 'none' }}>
+                                  {Comdata.c_title}
                                   </Link>
                               </td>
                               <td>
-                                {n.date}
+                                {Comdata.c_date}
                               </td>
                             </tr>
                             ))}
